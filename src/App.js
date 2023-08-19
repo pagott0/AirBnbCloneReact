67d9a31da2d5c4392863efd81f57f7346ac37dc0
pagott0 <enzovaspagotto@gmail.com>
2023-08-19 17:18:34 -0300

    Added the dynamic cards creation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import Navbar from "./components/Navbar";
 import Hero from './components/Hero';
 import Card from "./components/Card";
+import data from "./components/data";
+
 
 function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Card 
+        rate={item.stats.rating}
+        reviews={item.stats.reviewCount} 
+        country={item.location} 
+        img={item.coverImg}
+        price={136}
+        title={item.title}
+      />
+    )
+  })
+
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card rate="5.0" reviews="6" country="USA" img={require('./images/katie-zaferes.png')}/>
+      {cards}
       
+
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,15 +5,15 @@ import ImgStar from '../images/star.png';
 export default function Card(props) {
     return(
         <div className="card">
-            <img src ={props.img} alt="imgFromPerson" className="card--image"/>
+            <img src ={require(`../images/${props.img}`)} alt="imgFromPerson" className="card--image"/>
             <div className="cardStats">
                 <img src = {ImgStar} alt="StarImage" className="starImg"/>
                 <span>{props.rate}</span>
                 <span>({props.reviews})</span>
                 <span>{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p>From $136 / person</p>
+            <p>{props.title}</p>
+            <p>From ${props.price} / person</p>
         </div>
     )
 }
